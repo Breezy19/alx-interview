e40c171198b0cfb210435d47806d2ee199d63e9e
“Breezy19” <freij.akram@gmail.com>
2024-02-15 10:25:58 -0800

    StarWarsAPI

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log('Usage: ./0-starwars_characters.js [movieId]');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+  } else {
+    const film = JSON.parse(body);
+    const charactersUrls = film.characters;
+
+    charactersUrls.forEach(characterUrl => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error('Error:', error);
+        } else if (response.statusCode !== 200) {
+          console.error('Status:', response.statusCode);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  }
+});
